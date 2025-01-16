048ce4f727d0a78313ddd5e0e8dd89994f166a71
Asger Thorsboe Lundblad <atl@pentia.dk>
2025-01-16 11:10:57 +0100

    update the matcher to match the new api route structure so the protected routes are once again protected

--- src/middleware.ts
+++ src/middleware.ts
@@ -57,5 +57,5 @@ export const middleware = async (request: NextRequest) => {
 
 // Configure matcher to apply middleware only to specific routes
 export const config = {
-    matcher: ["/api/app/protected/:path*"], // Apply middleware to all routes in the api/app/protected directory
+    matcher: ["/app/api/v1/app/protected/:path*"], // Apply middleware to all routes in the api/app/protected directory
 };
